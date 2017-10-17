3d5652ed13cf8d6a74352d81493a91e71976b9d5
Dave Everitt <deveri00@LEC-ADMIN.dmu.ac.uk>
2017-10-17 15:36:52 +0100

    added h1 to html

--- js/greeting-nocomments.js
+++ js/greeting-nocomments.js
@@ -2,9 +2,13 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let myHeader = document.getElementById("my-header");
+  // ADD ANOTHER GETELEMENTBYID
   
   function PerformGreeting() {
     myName.innerHTML = userName.value;
+	myHeader.innerHTML = userName.value;
+    // copy line above but use the variable holding your h1 ID 
     event.preventDefault();
     return false;
   }
